af6dcc9f8ca1e746f227d3705edb8022d88e4f4c
POSADMIN <POSADMIN@DESKTOP-E0UJGU7>
2024-05-06 19:50:22 +0300

    Add project files.

---
+++ MultithreadDownloader/DownloadController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Globalization;
+
+namespace MultithreadDownloader
+{
+    internal class DownloadController
+    {
+
+        private string Filename;
+        private string URL;
+
+        HttpWebRequest request;
+        WebResponse responce;
+        private int TNumber;
+        private long BytesLength;
+        private long SectionLength;
+        private long LastPiece;
+        private List<DownloadThread> ThreadList = new List<DownloadThread>();
+        public DownloadController(string _filename, string _url, int _tnum, string path = "")
+        {
+            Filename = _filename;
+            URL = _url;
+            TNumber = _tnum;
+
+            request = (HttpWebRequest)WebRequest.Create(URL);
+            responce = request.GetResponse();
+            BytesLength = responce.ContentLength;
+
+        }
+
+        public void PrintData()
+        {
+
+            SplitIntoSections();
+            Console.WriteLine(URL);
+            Console.WriteLine($"Length: {BytesLength} bytes ~= {BytesLength / 1024 / 1024} Mb");
+            Console.WriteLine($"Number of threads: {TNumber}");
+            Console.WriteLine("Downloading");
+
+            Start();
+            Console.WriteLine("Downloaded");
+
+
+        }
+        public void SplitIntoSections()
+        {
+            SectionLength = BytesLength / TNumber;
+            LastPiece = BytesLength % TNumber;
+            for (int i = 0; i < TNumber; i++)
+            {
+                ThreadList.Add(new DownloadThread(i * SectionLength, ((i + 1) * SectionLength) - 1, $"{Filename}_temp_{i}"));
+            }
+            ThreadList[TNumber - 1].End = ThreadList[TNumber - 1].End + LastPiece + 1;
+
+        }
+        public async Task Start()
+        {
+            foreach (DownloadThread thread in ThreadList)
+            {
+                StartThreadAsync(thread);
+            }
+
+        }
+
+        public async Task StartThreadAsync(DownloadThread thread)
+        {
+            HttpWebRequest ThreadRequest = (HttpWebRequest)WebRequest.Create(URL);
+            ThreadRequest.AddRange(thread.Start, thread.End);
+            WebResponse ThreadResponce = (WebResponse)ThreadRequest.GetResponse();
+
+            using (Stream ThreadRespStream = ThreadResponce.GetResponseStream())
+            {
+                using (FileStream fs = new FileStream(thread.ThreadName, FileMode.Create))
+                {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = 0;
+                    do
+                    {
+                        bytesRead = ThreadRespStream.Read(buffer, 0, buffer.Length);
+                        fs.Write(buffer, 0, bytesRead);
+                        fs.Flush();
+                    }
+                    while (bytesRead > 0);
+                    fs.Close();
+                }
+            }
+            Console.WriteLine("Thread completed");
+        }
+
+        public void CombineTempFiles()
+        {
+
+            using (FileStream OutFile = new FileStream($"{Filename}", FileMode.Append, FileAccess.Write))
+            {
+                for (int i = 0; i < TNumber; i++)
+                {
+                    int bytesread = 0;
+                    byte[] buffer = new byte[1024];
+                    using (FileStream InputFile = new FileStream($"{Filename}_temp_{i}", FileMode.Open, FileAccess.Read))
+                    {
+                        do
+                        {
+                            bytesread = InputFile.Read(buffer, 0, buffer.Length);
+                            OutFile.Write(buffer, 0, bytesread);
+                        }
+                        while (bytesread > 0);
+                    }
+
+                }
+            }
+        }
+
+        public void DeleteTempFiles()
+        {
+            string[] TempFiles = Directory.GetFiles(Directory.GetCurrentDirectory(), "*temp*");
+            foreach (string TempFile in TempFiles)
+            {
+                File.Delete(TempFile);
+            }
+        }
+
+    }
+}
---
+++ MultithreadDownloader/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+///eer
+namespace MultithreadDownloader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string gg;
+            string link = "https://file-examples.com/storage/fe4996602366316ffa06467/2017/04/file_example_MP4_1280_10MG.mp4";
+            DownloadController dnl = new DownloadController("testfile.mp4", link, 4);
+            dnl.PrintData();
+            ///dnl.Start();
+            ///dnl.CombineTempFiles(); 
+            ///dnl.DeleteTempFiles();
+            Console.ReadLine();
+        }
+
+
+
+    }
+}
